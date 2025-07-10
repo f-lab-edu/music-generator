84fa9c8968ed060e9abd20111933c285024ccf8b
simuelunbo <simuelunbo@gmail.com>
2025-07-10 21:08:22 +0900

    build: build-logic 모듈 포함 및 프로젝트 설정 업데이트

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,5 @@
 pluginManagement {
+    includeBuild("build-logic")
     repositories {
         google {
             content {
@@ -11,7 +12,7 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-dependencyResolutionManagement {
+@Suppress("UnstableApiUsage") dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
@@ -19,5 +20,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Music generator"
+rootProject.name = "MusicGenerator"
 include(":app")
